d00c39c6c7c7d2ebf3e4cb1027ee8bfea0e92b98
Mikhail Salnikov <2613180+MihailSalnikov@users.noreply.github.com>
2024-12-02 16:36:42 +0300

    better

--- detoxer_run.sh
+++ detoxer_run.sh
@@ -1,5 +1,6 @@
-for lang in es hi ru uk zh en am ar de es hi ru uk zh 
+for lang in en
 do
-    python detoxer.py --language $lang --model_path "mlabonne/Meta-Llama-3.1-8B-Instruct-abliterated" --nshot 10 --batch_size 8 \
-    --output_path ./Meta-Llama-3.1-8B-Instruct_abliterated_10shot_t08_p09_from_multilingual_toxicity_dataset_toxpart_$lang.csv
+    python detoxer.py --language $lang --model_path "mlabonne/Meta-Llama-3.1-8B-Instruct-abliterated" \
+    --nshot 10 --batch_size 8 \
+    --output_path ./Meta-Llama-3.1-8B-Instruct_abliterated_10shot_$lang.csv
 done
--- trainer_run.sh
+++ trainer_run.sh
@@ -5,6 +5,5 @@ export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 
-python src/bart_baseline.py --config_path configs/bart_dolphin.json --input_columns en_toxic_comment --output_columns generated_neutral_sentence
-
-#python src/bart_baseline.py --config_path configs/bart_dolphin.json --input_columns en_toxic_comment --output_columns generated_neutral_sentence
+python src/bart_baseline.py --config_path configs/bart_real_train.json --input_columns en_toxic_comment --output_columns en_neutral_sentence
+python src/bart_baseline.py --config_path configs/bart_synth_train.json --input_columns en_toxic_comment --output_columns generated_neutral_sentence
